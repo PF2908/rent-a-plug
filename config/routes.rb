c2cc32ecfb8e9308533e0b3807f5bc3804736843
Gregory Bert <gregory_bert@yahoo.com>
2022-06-06 15:37:21 +0200

    odel edited and controller created

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'ads#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/rentals/ads/new', to: 'rentals#new'
+  post '/rentals/ads/', to: 'rentals#create'
 end
