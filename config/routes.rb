45de14522159fa689b40698bdd20e579b9fe5307
Gregory Bert <gregory_bert@yahoo.com>
2022-06-06 16:39:21 +0200

    rental branch

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'ads#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/rentals/ads/new', to: 'rentals#new'
-  post '/rentals/ads/', to: 'rentals#create'
+  # get '/rentals/ads/new', to: 'rentals#new'
+  # post '/rentals/ads/', to: 'rentals#create'
+  resources :rentals, only: [:new, :create]
 end
