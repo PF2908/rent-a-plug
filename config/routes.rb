b22060ffcc4eba4c15831c955e206b099a9f71eb
Paul Fournier <PaulF@MacBook-Air-de-Irvin.local>
2022-06-07 11:16:47 +0200

    model users last verison

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'users#index'
+  root to: 'ads#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :ads do
-    resources :rentals, only: [:new, :create, :destoy]
+    resources :rentals, only: [:new, :create, :destroy]
     resources :ratings, only: [:create, :destroy]
   end
 end
