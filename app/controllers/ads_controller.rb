6762fccdc984abf714a7e77cd3e57e8c4049a8fa
Gregory Bert <gregory_bert@yahoo.com>
2022-06-09 14:25:46 +0200

    searchbar with PG search working

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -20,14 +20,7 @@ class AdsController < ApplicationController
     end
 
     if params[:query].present?
-      sql_query = " \
-        ads.title @@ :query \
-        OR ads.description @@ :query \
-        OR ads.location @@ :query \
-        OR ads.color @@ :query \
-        OR ads.brand @@ :query \
-      "
-      @ads = Ad.where(sql_query, query: "%#{params[:query]}%")
+      @ads = Ad.global_search(params[:query])
     else
       @ads = Ad.all
     end
