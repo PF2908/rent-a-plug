a9522ee1648f0a0a5f44ed31c258115713ebf3a1
Serci Serge <serge.serci@hotmail.fr>
2022-06-10 16:33:28 +0200

    fix

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,8 +1,8 @@
 class Ad < ApplicationRecord
   has_one_attached :photo
   belongs_to :user
-  has_many :ratings
-  has_many :rentals
+  has_many :ratings, :dependent => :destroy
+  has_many :rentals, :dependent => :destroy
   validates :title, :location, :color, :cable_length, :brand, :number_of_plug, :price, presence: true
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
