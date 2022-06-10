ab36bf96d350d45865e604672050b2605ec45cb5
Philippine Burnichon <philippine_burnichon@icloud.com>
2022-06-10 15:58:02 +0200

    notice et alert delete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_09_123851) do
+ActiveRecord::Schema.define(version: 2022_06_08_120904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,19 +55,10 @@ ActiveRecord::Schema.define(version: 2022_06_09_123851) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
-    t.float "price"
     t.float "latitude"
     t.float "longitude"
+    t.float "price"
     t.index ["user_id"], name: "index_ads_on_user_id"
-  end
-
-  create_table "pg_search_documents", force: :cascade do |t|
-    t.text "content"
-    t.string "searchable_type"
-    t.bigint "searchable_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
   end
 
   create_table "ratings", force: :cascade do |t|
